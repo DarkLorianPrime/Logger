4f55e16b989ad4d29f43b61a5dc7a4b96853b554
DarkLorian <66025673+DarkLorianPrime@users.noreply.github.com>
2021-05-18 01:12:20 +0400

    Added some fixes, removed the example, now it's like a regular library

--- logger.py
+++ logger.py
@@ -1,5 +1,4 @@
 import os
-import re
 import time
 import traceback
 
@@ -8,26 +7,20 @@ def logger(info=None):
     rw = open(f'{os.getcwd()}/Logs.log', 'a+')
     if info is not None:
         rw.write(f'{"=" * 10}\n')
-        rw.write(f'{time.strftime("/%H:%M:%S/")} [INFO] {info} \n')
-        rw.write(f'{"=" * 10}\n')
+        rw.write(f'{time.strftime("/%H:%M:%S/")} [Function Loaded] name: {info} \n')
 
     def logger_dec(fn):
-
         def logger_wrap(*args):
-            returned_str = None
+            rw = open(f'{os.getcwd()}/Logs.log', 'a+')
             rw.write(f'{"=" * 10}\n')
             try:
                 returned = fn(*args)
+                print(f'{fn.__name__} successful execute.')
                 rw.write(f'{time.strftime("/%H:%M:%S/")} [LOADED] {fn.__name__} successful execute. \n')
-            except Exception as ex:
+            except Exception:
                 returned = args
-                r = traceback.format_exc().split(', ')
-                for i in r:
-                    if re.search('line', i):
-                        returned_str = f'{i}'
-                        break
                 rw.write(
-                    f'{time.strftime("/%H:%M:%S/")} [ERROR] Function [{fn.__name__}] Loading error: \n {ex} in {returned_str} \n')
+                    f'{time.strftime("/%H:%M:%S/")} [ERROR] Function [{fn.__name__}] Loading error: \n {traceback.format_exc()} \n')
                 print(
                     '[ERROR]Error, look at the information in the logs!\n[ERROR]Function returned the original value!')
             rw.write(f'{"=" * 10}\n')
